2f9db40b188af1200022e502f1067f19331a8bda
Aleggg <olehhasii@gmail.com>
2021-07-18 14:14:59 +0300

    Added links and small changes

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeMenu = document.querySelector('.menu__close');
+    menuLink = document.querySelectorAll('.menu__link');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -8,4 +9,12 @@ hamburger.addEventListener('click', () => {
 
 closeMenu.addEventListener('click', () => {
     menu.classList.remove('active');
+});
+
+
+
+menuLink.forEach(element => {
+    element.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
 });
